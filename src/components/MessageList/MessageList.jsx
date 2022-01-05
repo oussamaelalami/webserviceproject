8630b86eda33d71046cca040fce94a6753810720
unknown <oussamaelalami1999@gmail.com>
2022-01-05 13:58:10 +0100

    We finished!

---
+++ src/components/MessageList/MessageList.jsx
@@ -0,0 +1,56 @@
+import { useChat } from 'context';
+import { ChatAvatar } from 'components';
+import { groupMessages } from 'helpers';
+import { useScrollToBottom } from 'hooks';
+import { format } from 'timeago.js';
+
+export const MessageList = () => {
+  const { selectedChat } = useChat();
+  useScrollToBottom(selectedChat, 'chat-messages');
+
+  return (
+    <div className="chat-messages">
+      {!!selectedChat.messages.length ? (
+        groupMessages(selectedChat.messages).map((m, index) => (
+          <div key={index} className="chat-message">
+            <div className="chat-message-header">
+              <ChatAvatar
+                className="message-avatar"
+                username={m[0].sender.username}
+                chat={selectedChat}
+              />
+              <div className="message-author">{m[0].sender.username}</div>
+            </div>
+
+            <div className="message-content">
+              {m.map((individualMessage, index) => (
+                <div key={index}>
+                  <div className="message-text">{individualMessage.text}</div>
+
+                  {!!individualMessage.attachments.length && (
+                    <a
+                      href={individualMessage.attachments[0].file}
+                      target="_blank"
+                    >
+                      <img
+                        className="message-image"
+                        src={individualMessage.attachments[0].file}
+                        alt={individualMessage.id + '-attachment'}
+                      />
+                    </a>
+                  )}
+                  <br />
+                  <div className="messageBottom">
+                    {format(m[index].created)}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className="no-messages-yet">No messages yet</div>
+      )}
+    </div>
+  );
+};
